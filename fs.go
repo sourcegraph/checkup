e01ad88422ff1dd27234fd7f4692d2789f5eaa38
emersion <contact@emersion.fr>
2016-08-13 15:26:29 +0200

    Adds index file to FS

--- fs.go
+++ fs.go
@@ -8,9 +8,13 @@ import (
 	"time"
 )
 
+const indexName = "index.json"
+
 // FS is a way to store checkup results on the local filesystem.
 type FS struct {
+	// The path to the directory where check files will be stored.
 	Dir string `json:"dir"`
+	// The URL corresponding to fs.Dir.
 	Url string `json:"url"`
 
 	// Check files older than CheckExpiry will be
@@ -20,15 +24,52 @@ type FS struct {
 	CheckExpiry time.Duration `json:"check_expiry,omitempty"`
 }
 
-// Store stores results on filesystem according to the configuration in fs.
-func (fs FS) Store(results []Result) error {
-	f, err := os.Create(filepath.Join(fs.Dir, *GenerateFilename()))
+func (fs FS) readIndex() (index map[string]int64, err error) {
+	f, err := os.Open(filepath.Join(fs.Dir, indexName))
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	err = json.NewDecoder(f).Decode(&index)
+	return
+}
+
+func (fs FS) writeIndex(index map[string]int64) error {
+	f, err := os.Create(filepath.Join(fs.Dir, indexName))
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
-	return json.NewEncoder(f).Encode(results)
+	return json.NewEncoder(f).Encode(index)
+}
+
+// Store stores results on filesystem according to the configuration in fs.
+func (fs FS) Store(results []Result) error {
+	// Write results to a new file
+	name := *GenerateFilename()
+	f, err := os.Create(filepath.Join(fs.Dir, name))
+	if err != nil {
+		return err
+	}
+	err = json.NewEncoder(f).Encode(results)
+	f.Close()
+	if err != nil {
+		return err
+	}
+
+	// Read current index file
+	index, err := fs.readIndex()
+	if err != nil {
+		return err
+	}
+
+	// Add new file to index
+	index[name] = time.Now().UnixNano()
+
+	// Write new index
+	return fs.writeIndex(index)
 }
 
 // Maintain deletes check files that are older than fs.CheckExpiry.
@@ -42,13 +83,25 @@ func (fs FS) Maintain() error {
 		return err
 	}
 
+	index, err := fs.readIndex()
+	if err != nil {
+		return err
+	}
+
 	for _, f := range files {
-		if time.Since(f.ModTime()) > fs.CheckExpiry {
+		name, _ := filepath.Rel(fs.Dir, f.Name())
+		if name == indexName {
+			continue
+		}
+
+		nsec, ok := index[name]
+		if !ok || time.Since(time.Unix(0, nsec)) > fs.CheckExpiry {
 			if err := os.Remove(f.Name()); err != nil {
 				return err
 			}
+			delete(index, name)
 		}
 	}
 
-	return nil
+	return fs.writeIndex(index)
 }
