1f7fbc08ba1da0bddb85744b8abec57b12f7ab93
Magnus Udbjørg <mud@codehouse.com>
2017-12-19 18:47:44 +0100

    Simplified HttpsChecker using inhritance based on httpchecker

--- httpschecker.go
+++ httpschecker.go
@@ -2,8 +2,6 @@ package checkup
 
 import (
 	"crypto/tls"
-	"fmt"
-	"io/ioutil"
 	"net"
 	"net/http"
 	"strings"
@@ -12,59 +10,7 @@ import (
 
 // HTTPSChecker implements a Checker for HTTP endpoints.
 type HTTPSChecker struct {
-	// Name is the name of the endpoint.
-	Name string `json:"endpoint_name"`
-
-	// URL is the URL of the endpoint.
-	URL string `json:"endpoint_url"`
-
-	// UpStatus is the HTTP status code expected by
-	// a healthy endpoint. Default is http.StatusOK.
-	UpStatus int `json:"up_status,omitempty"`
-
-	// ThresholdRTT is the maximum round trip time to
-	// allow for a healthy endpoint. If non-zero and a
-	// request takes longer than ThresholdRTT, the
-	// endpoint will be considered unhealthy. Note that
-	// this duration includes any in-between network
-	// latency.
-	ThresholdRTT time.Duration `json:"threshold_rtt,omitempty"`
-
-	// MustContain is a string that the response body
-	// must contain in order to be considered up.
-	// NOTE: If set, the entire response body will
-	// be consumed, which has the potential of using
-	// lots of memory and slowing down checks if the
-	// response body is large.
-	MustContain string `json:"must_contain,omitempty"`
-
-	// MustNotContain is a string that the response
-	// body must NOT contain in order to be considered
-	// up. If both MustContain and MustNotContain are
-	// set, they are and-ed together. NOTE: If set,
-	// the entire response body will be consumed, which
-	// has the potential of using lots of memory and
-	// slowing down checks if the response body is large.
-	MustNotContain string `json:"must_not_contain,omitempty"`
-
-	// Attempts is how many requests the client will
-	// make to the endpoint in a single check.
-	Attempts int `json:"attempts,omitempty"`
-
-	// AttemptSpacing spaces out each attempt in a check
-	// by this duration to avoid hitting a remote too
-	// quickly in succession. By default, no waiting
-	// occurs between attempts.
-	AttemptSpacing time.Duration `json:"attempt_spacing,omitempty"`
-
-	// Client is the http.Client with which to make
-	// requests. If not set, DefaultHTTPSClient is
-	// used.
-	Client *http.Client `json:"-"`
-
-	// Headers contains headers to added to the request
-	// that is sent for the check
-	Headers http.Header `json:"headers,omitempty"`
+	*HTTPChecker
 }
 
 // Check performs checks using c according to its configuration.
@@ -82,6 +28,7 @@ func (c HTTPSChecker) Check() (Result, error) {
 
 	result := Result{Title: c.Name, Endpoint: c.URL, Timestamp: Timestamp()}
 	req, err := http.NewRequest("GET", c.URL, nil)
+
 	if err != nil {
 		return result, err
 	}
@@ -99,86 +46,6 @@ func (c HTTPSChecker) Check() (Result, error) {
 	result.Times = c.doChecks(req)
 
 	return c.conclude(result), nil
-}
-
-// doChecks executes req using c.Client and returns each attempt.
-func (c HTTPSChecker) doChecks(req *http.Request) Attempts {
-	checks := make(Attempts, c.Attempts)
-	for i := 0; i < c.Attempts; i++ {
-		start := time.Now()
-		resp, err := c.Client.Do(req)
-		checks[i].RTT = time.Since(start)
-		if err != nil {
-			checks[i].Error = err.Error()
-			continue
-		}
-		err = c.checkDown(resp)
-		if err != nil {
-			checks[i].Error = err.Error()
-		}
-		resp.Body.Close()
-		if c.AttemptSpacing > 0 {
-			time.Sleep(c.AttemptSpacing)
-		}
-	}
-	return checks
-}
-
-// conclude takes the data in result from the attempts and
-// computes remaining values needed to fill out the result.
-// It detects degraded (high-latency) responses and makes
-// the conclusion about the result's status.
-func (c HTTPSChecker) conclude(result Result) Result {
-	result.ThresholdRTT = c.ThresholdRTT
-
-	// Check errors (down)
-	for i := range result.Times {
-		if result.Times[i].Error != "" {
-			result.Down = true
-			return result
-		}
-	}
-
-	// Check round trip time (degraded)
-	if c.ThresholdRTT > 0 {
-		stats := result.ComputeStats()
-		if stats.Median > c.ThresholdRTT {
-			result.Notice = fmt.Sprintf("median round trip time exceeded threshold (%s)", c.ThresholdRTT)
-			result.Degraded = true
-			return result
-		}
-	}
-
-	result.Healthy = true
-	return result
-}
-
-// checkDown checks whether the endpoint is down based on resp and
-// the configuration of c. It returns a non-nil error if down.
-// Note that it does not check for degraded response.
-func (c HTTPSChecker) checkDown(resp *http.Response) error {
-	// Check status code
-	if resp.StatusCode != c.UpStatus {
-		return fmt.Errorf("response status %s", resp.Status)
-	}
-
-	// Check response body
-	if c.MustContain == "" && c.MustNotContain == "" {
-		return nil
-	}
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("reading response body: %v", err)
-	}
-	body := string(bodyBytes)
-	if c.MustContain != "" && !strings.Contains(body, c.MustContain) {
-		return fmt.Errorf("response does not contain '%s'", c.MustContain)
-	}
-	if c.MustNotContain != "" && strings.Contains(body, c.MustNotContain) {
-		return fmt.Errorf("response contains '%s'", c.MustNotContain)
-	}
-
-	return nil
 }
 
 // DefaultHTTPSClient is used when no other http.Client
